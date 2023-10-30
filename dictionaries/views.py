7763780e8bc28176439819e7eaa39a950eb04743
Michał <mi.ke@o2.pl>
2023-10-30 19:40:10 +0100

    Added dictionary import page

--- dictionaries/views.py
+++ dictionaries/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, HttpResponse
 from .dictionary_importer import import_dictionary
+from .models import Dictionary
 
 # Create your views here.
 def dictionary_import(request):
     
-    
+    if request.method == "POST":
+        print(request.FILES)
     
     #import_dictionary("./dictionaries/jmdict_english.zip")
+    dictionaries = Dictionary.objects.all()
 
-    return render(request, "import.html")
+    return render(request, "import.html", {"dictionaries": dictionaries})
